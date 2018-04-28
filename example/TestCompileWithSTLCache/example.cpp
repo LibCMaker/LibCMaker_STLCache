247b2d74fdac2386ecb645c4f0e0b6747a8c7fcf
NikitaFeodonit <nfeodonit@yandex.com>
2018-04-28 13:39:55 +0300

    Add all policies to example.

--- example/TestCompileWithSTLCache/example.cpp
+++ example/TestCompileWithSTLCache/example.cpp
@@ -7,22 +7,29 @@
 
 int main(int argc, char* argv[])
 {
-  stlcache::cache<int, std::string, stlcache::policy_lru> c1(3);
+  stlcache::cache<int, std::string, stlcache::policy_none> c0(3);
+  stlcache::cache<int, std::string, stlcache::policy_adaptive> c1(3);
+  stlcache::cache<int, std::string, stlcache::policy_lfu> c2(3);
+  stlcache::cache<int, std::string, stlcache::policy_lfuaging<3>> c3(3);
+  stlcache::cache<int, std::string, stlcache::policy_lfuagingstar<3>> c4(3);
+  stlcache::cache<int, std::string, stlcache::policy_lfustar> c5(3);
+  stlcache::cache<int, std::string, stlcache::policy_lru> c6(3);
+  stlcache::cache<int, std::string, stlcache::policy_mru> c7(3);
 
-  c1.insert(1, "data1");
-  c1.insert(2, "data2");
-  c1.insert(3, "data3");
-  c1.insert(4, "data4");
+  c6.insert(1, "data1");
+  c6.insert(2, "data2");
+  c6.insert(3, "data3");
+  c6.insert(4, "data4");
 
-  if(c1.check(1)) {
+  if(c6.check(1)) {
     return 1;  // Must be removed by LRU policy
   }
 
-  c1.touch(2);
+  c6.touch(2);
 
-  c1.insert(5, "data5");
+  c6.insert(5, "data5");
 
-  if(c1.check(3)) {
+  if(c6.check(3)) {
     return 1;  // Must be removed by LRU policy (cause 2 is touched)
   }
 
